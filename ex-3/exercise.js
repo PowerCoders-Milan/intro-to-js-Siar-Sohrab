f503e6797872325f7d50f5fd249613f488a41156
Siar Sohrab <siar.sohrab@gmail.com>
2021-02-25 15:00:15 +0100

    done ex-3

--- ex-3/exercise.js
+++ ex-3/exercise.js
@@ -1,2 +1,12 @@
 // Example
-window.alert("Hello World!");
+var x = 100;
+var y = 45;
+if (x > y){
+    console.log('x is greater than y')
+}
+else if (x < y) {
+console.log('y is greater than x')
+}
+else{
+    console.log('x and y are equal')
+}
