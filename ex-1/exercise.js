429ca122c97b89646a11e87bf42b1b22c3881247
Siar Sohrab <siar.sohrab@gmail.com>
2021-02-25 12:22:21 +0100

    done ex-1

--- ex-1/exercise.js
+++ ex-1/exercise.js
@@ -1,2 +1,7 @@
 // Example
 window.alert("Hello World!");
+var x = "doctor";
+var y = "rome";
+var z = "mary";
+var n = 2;
+console.log( "you will be a " + x +" in " + y + ", and married to " + z + " with " + n + " kids.");
