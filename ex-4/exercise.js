2a256b83a5fab1223ccc5ae2f1edbf89ffc70871
Siar Sohrab <siar.sohrab@gmail.com>
2021-02-25 15:40:57 +0100

    done ex-4

--- ex-4/exercise.js
+++ ex-4/exercise.js
@@ -1,2 +1,35 @@
 // Example
-window.alert("Hello World!");
+
+
+var name = prompt("please write you name", 'name');
+
+window.alert('welcome ' + name);
+
+
+var day = prompt( "how was your day");
+if (day === "good"){
+    window.alert("I'm happy for you champ")
+}
+else if ( day === "bad"){
+    window.alert("That's too bad, tomorrow it's a new day")
+}
+
+
+
+
+
+
+
+
+
+
+
+
+//Create a program in JavaScript that prompts for your name and prints a greeting using your name.
+
+//Then the program asks how your day was.
+
+//You say either good or bad.
+
+//Then the program answers "I'm happy for you champ" or "That's too bad, tomorrow it's a new day" accordingly.
+//
